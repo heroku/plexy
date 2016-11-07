d5a714a8b69ba45cb4b099da49acd9f6fd1db2cc
Kevin Thompson <kdt@heroku.com>
2016-11-07 15:44:10 -0600

    Add tests for Instrumentor.call/2 at finish

--- test/plexy/instrumentor_test.exs
+++ test/plexy/instrumentor_test.exs
@@ -16,13 +16,28 @@ defmodule Plexy.InstrumentorTest do
 
   @opts Instrumentor.init([])
   test "Instrumentor.call/2 logs the start" do
-
     logged = capture_log(fn ->
-      conn = conn(:get, "/foobar/foo-az")
-             |> Instrumentor.call(@opts)
+      conn(:get, "/foobar") |> Instrumentor.call(@opts)
     end)
 
     assert logged =~ "instrumentation"
-    assert logged =~ "at: \"start\""
+    assert logged =~ "start"
+    assert logged =~ "path"
+    assert logged =~ "method"
+  end
+
+  test "Instrumentor.call/2 logs the finish" do
+    logged = capture_log(fn ->
+      conn(:get, "/foobar")
+      |> Instrumentor.call(@opts)
+      |> Plug.Conn.send_resp(200, "wow")
+    end)
+
+    assert logged =~ "instrumentation"
+    assert logged =~ "finish"
+    assert logged =~ "elapsed"
+    assert logged =~ "status"
+    assert logged =~ "path"
+    assert logged =~ "method"
   end
 end
